d8609f3740e7c6fbb781337cf30df5dfbf9b89eb
LuciaRyan <lcryan06@gmail.com>
2022-11-29 10:25:05 +0100

    point 6 completed - all nav elements are linking to their pages

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,21 +1,31 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
+
 import './Navigation.css';
 
 function Navigation() {
-  return (
-    <nav>
-      <div className="nav-container">
-        <h4>De Tandenborstel</h4>
+    return (
+        <nav>
+            <div className="nav-container">
+                <h4>De Tandenborstel</h4>
 
-        <ul>
-          <li>Home</li>
-          <li>Gaatjes</li>
-          <li>Afspraak maken</li>
-          <li>Tanden bleken</li>
-        </ul>
-      </div>
-    </nav>
-  );
+                <ul>
+                    <li>
+                        <NavLink to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/cavities">Gaatjes</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/appointments">Afspraak maken</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/whitening">Tanden bleken</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
 
 export default Navigation;
